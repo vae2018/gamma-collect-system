b566fdc455e50e503097ea6b7c54545736b73528
ye <1484531170@qq.com>
2018-10-19 15:54:39 +0800

    first

---
+++ build.js
@@ -0,0 +1,5 @@
+console.log('hello word!!!')
+console.log('hello word!!!')
+console.log('hello word!!!')
+console.log('hello word!!!')
+console.log('hello word!!!')
---
+++ haha.js
@@ -0,0 +1,8 @@
+console.log('123456')
+
+
+
+require['test.js',function(){
+
+console.log('666666666')
+}]
